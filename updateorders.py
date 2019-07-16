b2c48c97d4f6264c9ed1961f478c844b107e411c
David J. Fries <david.fries@greenfiber.com>
2019-07-16 14:40:05 -0400

    update orders instantiation fix

--- updateorders.py
+++ updateorders.py
@@ -1,8 +1,8 @@
-from integration import RoseRocketIntegration as rr 
+from integration import RoseRocketIntegration  
 from backend import RoseRocketIntegrationBackend
 from secretprod import secrets as pw
 
 
 if __name__ == "__main__":
-    
+    rr = RoseRocketIntegration()
     rr.updatesync()
