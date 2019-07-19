57f7c8007d7450058f514a70bb3e6e5e650d8e72
David J. Fries <david.fries@greenfiber.com>
2019-07-19 09:33:46 -0400

    working on sending carriers to rr

--- integration.py
+++ integration.py
@@ -19,9 +19,38 @@ class RoseRocketIntegration():
         self.whcode = whcode
 
     apiurl = ''
-    # warehousecode:warehousename
+    
+    def synccarriers(self,data):
+        import pandas as pd
+        df=pd.read_excel(io="C:\\Users\\friesdj\\Downloads\\Carrier Report.xlsx")
+        sheet=df.where((pd.notnull(df)),'none')
+        for index,row in sheet.iterrows():
+            params={
+            "external_id": row.Code,
+            "short_code": row.Code[:6],
+            "name": row.Name,
+            "address_1": row.Address1,
+            "address_2": row.Address2,
+            "is_active":True,
+            "city": row.City,
+            "postal": str(row.Zip),
+            "phone": row.Phone,
+            
+            
+            }
+            auth = self.authorg(self.whcode)
+        
+            headers = {
+                'Accept': 'application/json',
+                
 
-    # data=db.getAllData(whcode)
+
+            }
+            apiurl = 'https://platform.roserocket.com/v1/partner_carriers'
+            r = requests.post(
+                apiurl, json=params, headers=headers)
+            resp = r.json()
+        
 
     def logStart(self):
         logging.basicConfig(filename='C:\\rrsync\\sync.log', level=logging.DEBUG,
