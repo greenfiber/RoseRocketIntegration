53e6114f129419677d7f2256f9a528a25b826c42
David J. Fries <david.fries@greenfiber.com>
2019-09-08 23:29:10 -0400

    added freight charge lookup with order id

--- nickreport.py
+++ nickreport.py
@@ -1,102 +1,152 @@
-from integration import RoseRocketIntegration,RoseRocketIntegrationBackend
-from secret import secrets as pw
+from integration import RoseRocketIntegration, RoseRocketIntegrationBackend
+from secretprod import secrets as pw
 import requests
 import xlwings as xw
-
+import pprint
 orgs = pw.orgs.keys()
+choice=''
+while(choice !='YES'):
+    startdate=input("Enter start date (yyyymmdd):")
+    enddate=input("Enter end date (yyyymmdd):")
+    print("START: {} ||| END: {}".format(startdate,enddate))
+    choice=input("Are these dates correct? (YES/NO):")
+    choice=str(choice).upper()
 
-
-db= RoseRocketIntegrationBackend()
+db = RoseRocketIntegrationBackend()
+pddata = []
+missingorders=[]
+print("generating report...")
+counter = 0
 for org in orgs:
-    results=db.getOrderHistory(org)
-    rr=RoseRocketIntegration(org)
+    results = db.getOrderHistory(org,str(startdate),str(enddate))
+    rr = RoseRocketIntegration(org)
     auth = rr.authorg(org)
-    
-    
+
     headers = {
-                'Accept': 'application/json',
-                'Authorization': 'Bearer {}'.format(auth)
+        'Accept': 'application/json',
+        'Authorization': 'Bearer {}'.format(auth)
 
 
-            }
-    
-    pddata=[]
-    
+    }
+    apiurl = 'https://platform.roserocket.com/api/v1/bills'
+    bills = requests.get(apiurl, headers=headers).json()
+
     for result in results:
-        apiurl = 'https://platform.sandbox01.roserocket.com/api/v1/customers/ext:{}/orders/ext:{}'.format(result.ARDIVISIONNO+result.CUSTOMERNO,result.SALESORDERNO)
 
-        data={
-            "stationcode":''
-            ,"orderno":''
-            ,"housebill":''
-            ,"ofddate":''
-            ,"totalcost":''
-            ,"totalpieces":''
-            ,"routingvendor":''
-        
+        apiurl = 'https://platform.roserocket.com/api/v1/customers/ext:{}/orders/ext:{}'.format(
+            result.ARDIVISIONNO+result.CUSTOMERNO, result.SALESORDERNO)
+
+        data = {
+            "stationcode": '', "orderno": '', "housebill": '', "ofddate": '', "totalcost": '', "totalpieces": '', "routingvendor": ''
+
         }
         so = result.SALESORDERNO
-        
-        
-        #this gets the order id
+
+        # this gets the order id
         try:
-            resp=requests.get(apiurl,headers=headers).json()
+            resp = requests.get(apiurl, headers=headers).json()
+            # print(resp)
         except Exception as e:
             print("can't find order: {}".format(so))
             print(e)
             # continue
         # print(resp)
         if("error_code" not in resp):
-            orderids=[]
-            data['stationcode']=org
-            data['housebill']=so
-            
+            orderids = []
+            data['stationcode'] = org
+            data['housebill'] = so
+
             try:
-                id=resp["order"]['id']
-            
+                id = resp["order"]['id']
+
             except:
                 print("\n \n")
                 print(resp)
                 print("\n \n")
-            data['orderno']=id 
+            data['orderno'] = id
         else:
+            missingorders.append(so)
+            print(resp)
+            print(so)
+            print(org)
             continue
-       
 
-        
-        
-        apiurl = 'https://platform.sandbox01.roserocket.com/api/v1/orders/{}/legs'.format(id)
-        resp = requests.get(apiurl,headers=headers).json()
-        legs=[]
-        #legs
+        apiurl = 'https://platform.roserocket.com/api/v1/orders/{}/legs'.format(
+            id)
+        resp = requests.get(apiurl, headers=headers).json()
+        # print(resp)
+        legs = []
+        # legs
+
         for leg in resp["legs"]:
-            totalweight = 0
-            totalpieces=0
-            data['ofddate']=leg["history"]["destination_delivered_at"]
-            manifestid = leg["manifest_id"]
-            #commodities in each leg
-            for comm in leg["commodities"]:
-                print(comm["weight"])
-                print(comm["pieces"])
-                print(comm["quantity"])
-                totalweight += comm["weight"]
-                if(comm["pieces"]==None):
-                    totalpieces += comm["quantity"]
-                else:
-                    totalpieces += comm["pieces"]
-                
-                
-                #getting manifestid for use to get manifests
-                apiurl = 'https://platform.sandbox01.roserocket.com/api/v1/manifests/{}'.format(manifestid)
-                resp=requests.get(apiurl,headers=headers).json()
-                carrierid=resp["manifest"]["partner_carrier_id"]
-                #manifest is used to get partner carrier id
-                apiurl = 'https://platform.sandbox01.roserocket.com/api/v1/partner_carriers/{}'.format(carrierid)
-                resp = requests.get(apiurl,headers=headers).json()
-                #finally with the partner carrier id you can get the parner carrier name
-                data['routingvendor']=resp["partner_carrier"]["name"]
-        pddata.append(data)
-print(pddata)
 
+            if(leg["manifest_id"] != None and leg["history"]["origin_pickedup_at"] != None):
+                totalweight = 0
+                totalpieces = 0
+                data['ofddate'] = leg["history"]["origin_pickedup_at"]
+                # print(leg)
+                manifestid = leg["manifest_id"]
+                # commodities in each leg
+                for comm in leg["commodities"]:
+                    # print(comm["weight"])
+                    # print(comm["pieces"])
+                    # print(comm["quantity"])
 
+                    if(comm["quantity"] == 1):
+                        totalpieces += comm["pieces"]
+                        data['totalpieces'] = totalpieces
+                        totalweight = comm["weight"]
+                        data['totalweight'] = totalweight
+                    
+                    elif(comm["pieces"] == 1):
+                        totalpieces += comm["quantity"]
+                        data['totalpieces'] = totalpieces
+                        totalweight = comm["weight"]*totalpieces
+                        data['totalweight'] = totalweight
+                    elif(comm["quantity"] >=1):
+                        totalpieces += comm["pieces"]
+                        data['totalpieces'] = totalpieces
+                        totalweight = comm["weight"]*comm["quantity"]
+                        data['totalweight'] = totalweight
 
+                    # getting manifestid for use to get manifests
+                apiurl = 'https://platform.roserocket.com/api/v1/manifests/{}/payment'.format(
+                    manifestid)
+                resp = requests.get(apiurl, headers=headers).json()
+                #get estimated cost
+                data['totalcost'] = resp["manifest_payment"]["sub_total_amount"]
+                # get partner carrierid
+                apiurl = 'https://platform.roserocket.com/api/v1/manifests/{}/'.format(
+                    manifestid)
+                resp = requests.get(apiurl, headers=headers).json()
+
+                carrierid = resp["manifest"]["partner_carrier_id"]
+                # manifest is used to get partner carrier id
+                apiurl = 'https://platform.roserocket.com/api/v1/partner_carriers/{}'.format(
+                    carrierid)
+                resp = requests.get(apiurl, headers=headers).json()
+                # finally with the partner carrier id you can get the parner carrier name
+                try:
+
+                    data['routingvendor'] = resp["partner_carrier"]["name"]
+                except:
+                    data['routingvendor'] = "NULL"
+
+                counter += 1
+                pddata.append(data)
+                print("orders processed {}".format(counter))
+            else:
+                pass
+#                 print(
+#                     "order not added to report because it hasn't shipped yet {}".format(so))
+
+# print(pddata)
+import pandas as pd
+df = pd.DataFrame(pddata)
+
+path=r"Y:/shipmentreport.xlsx"
+wb = xw.Book()
+sheet = wb.sheets['Sheet1']
+sheet.range('A1').value = df
+
+print("MISSING ORDERS ON RR {}".format(len(missingorders)))
--- rrtosage.py
+++ rrtosage.py
@@ -1,20 +1,57 @@
 from flask import Flask, request, render_template, redirect, flash, session, abort, jsonify
 from flask_api import status
+import requests
+from integration import RoseRocketIntegration
 import os
 app = Flask(__name__)
 app.secret_key = os.urandom(12)
+
+def getfreightamt(orderid):
+    rr = RoseRocketIntegration(org)
+    auth = rr.authorg(org)
+
+    headers = {
+        'Accept': 'application/json',
+        'Authorization': 'Bearer {}'.format(auth)
+
+
+    }
+    apiurl = 'https://sandbox01.platform.roserocket.com/api/v1/orders/{}/legs'.format(
+            id)
+    resp = requests.get(apiurl, headers=headers).json()
+    # print(resp)
+    legs = []
+    # legs
+
+    for leg in resp["legs"]:
+
+        if(leg["manifest_id"] != None and leg["history"]["origin_pickedup_at"] != None):
+            
+           
+            manifestid = leg["manifest_id"]
+            
+            # getting manifestid for use to get manifests
+            apiurl = 'https://sandbox01.platform.roserocket.com/api/v1/manifests/{}/payment'.format(
+                manifestid)
+            resp = requests.get(apiurl, headers=headers).json()
+            #get estimated cost
+            return = resp["manifest_payment"]["sub_total_amount"]
 
 @app.route('/', methods=['GET','POST'])
 def default():
     token = request.args.get('token')
     if(token == 'CTq74c42cuUMkudJbPVF3GsH'):
         print(request.data)
+        try:
+            freightcharge = getfreightamt(request.args.get('org'))
+        except:
+            freightcharge = "error in freight charge lookup for order {}".format(request.data['order_id'])
         # if(request.method == 'GET'):
         #     print(request.data)
         #     return status.HTTP_200_OK
         # else:
         #     print(request.data)
-        return str(request.data)
+        return str(str(freightcharge))
 
     else:
         return 'Not okay auth'
